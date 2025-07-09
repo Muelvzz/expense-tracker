2cf461374f95412fac18d087b8742b9b322e4f97
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-09 17:21:51 +0800

    add 'edit'

--- main.py
+++ main.py
@@ -8,15 +8,58 @@ print("EXPENSE TRACKER")
 
 while True:
     try:
-        user_entry = input("Enter the expenses: ")
-        entry_list = list(user_entry.split(" "))
-        description = entry_list[0].capitalize()
-        amount = int(entry_list[1])
 
-        with open(file, "a", newline="") as file:
-            file.write(f"{description};{amount};{time}\n")
+        user_action = input("Type add, show, edit, or exit: ")
+
+        if user_action.startswith("add"):
+
+            user_entry = input("Enter the expenses: ")
+            entry_list = list(user_entry.split(" "))
+            description = entry_list[0].capitalize()
+            amount = int(entry_list[1])
+
+            with open(file, "a", newline="") as f:
+                f.write(f"{description};{amount};{time}\n")
+
+        elif user_action.startswith("edit"):
+
+            with open(file, "r") as f:
+                df = pd.read_csv(f, sep=";")
+
+                df.index += 1
+
+                print(df)
+                print(" ")
+                try:
+                    user_edit = int(input("Enter the number to edit: "))
+
+                    if user_edit in df.index:
+                        new_entry = input("Enter the new value using this format [desctription] [amount]: ")
+                        new_entry_list = list(new_entry.split(" "))
+
+                        new_description = new_entry_list[0].capitalize()
+                        new_amount = int(new_entry_list[1])
+
+                        df.at[user_edit, 'description'] = new_description
+                        df.at[user_edit, 'amount'] = new_amount
+                        df.at[user_edit, 'date'] = time
+
+                        df.to_csv("data.csv", sep=";", index=False)
+
+                        print("successfully updated")
+
+                    else:
+                        print("The number is out of the index")
+                    
+                except (ValueError, IndexError):
+                    print("Invalid input")
+
+
+        elif user_action.startswith("exit"):
+            break
+
+        else:
+            print("please enter add, show, edit or exit")
 
     except TypeError:
         print("Sorry, the format should be [description] [amount]")
-    
-    break
